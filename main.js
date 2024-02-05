b2372670a2ce6c0ab7eb1cdea4dc97f93dda773a
Filipe <filipe.raitz@hotmail.com>
2024-02-05 14:57:09 -0400

    alterações finais

--- main.js
+++ main.js
@@ -3,5 +3,27 @@ $(function(){
     $('#telefone').mask('(00) 00000-0000')
     $('#cep').mask('00000-000')
 
-
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereço: {
+                required: true
+            }
+        },
+    })
 })
